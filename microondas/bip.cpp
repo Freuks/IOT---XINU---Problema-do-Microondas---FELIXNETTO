ae473b093eb4ac16647636777b4d98d09da7b0b8
Félix Netto <112364180+Freuks@users.noreply.github.com>
2024-11-22 18:35:14 -0300

    Add files via upload

---
+++ microondas/bip.cpp
@@ -0,0 +1,24 @@
+#include <xinu.h>
+
+void beep() {
+    kprintf("\a");
+}
+
+process main(void) {
+    int timer;
+    char inputBuffer[10];
+
+    kprintf("Digite o tempo do timer em segundos: ");
+    read(CONSOLE, inputBuffer, sizeof(inputBuffer) - 1)
+    inputBuffer[9] = '\0';
+    timer = atoi(inputBuffer);
+
+    while (timer > 0) {
+        kprintf("Tempo restante: %d segundos\n", timer);
+        sleep(1);
+        timer--;
+    }
+    kprintf("BIP BIP BIP\n");
+    beep();
+    return OK;
+}
