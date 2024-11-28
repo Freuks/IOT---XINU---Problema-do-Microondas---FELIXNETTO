1f2d2ae209e3cde6b092090d1a682f9f9dc8976d
Félix Netto <112364180+Freuks@users.noreply.github.com>
2024-11-28 10:35:02 -0300

    Update cliclos_de_pratos.cpp

--- microondas/cliclos_de_pratos.cpp
+++ microondas/cliclos_de_pratos.cpp
@@ -1,4 +1,5 @@
 #include <xinu.h>
+#include <stdlib.h>
 
 typedef struct {
     int tempo;
@@ -12,29 +13,37 @@ void menu(Prato pratos[], int num_pratos) {
 }
 void aquecer_prato(Prato prato) {
     kprintf("Aquecendo %s por %d segundos...\n", prato.nome, prato.tempo);
-    for (int i = prato.tempo; i > 0; i--) {
+    for (int i=prato.tempo; i > 0; i--) {
         kprintf("%d segundos restantes...\n", i);
         sleepms(1000);
     }
     kprintf("Aquecimento de %s concluido!\n", prato.nome);
 }
-process main() {
+shellcmd xsh_ciclos(int nargs, char *args[]) {
     Prato pratos[] = {
-        {"Pipoca", 120},
-        {"Lasanha", 180},
-        {"Arroz", 300},
-        {"Pizza", 240},
-        {"Frango", 240},
-        {"Legumes", 150},
+        {.nome = "Pipoca", .tempo = 120},
+        {.nome = "Lasanha", .tempo = 180},
+        {.nome = "Arroz", .tempo = 300},
+        {.nome = "Pizza", .tempo = 240},
+        {.nome = "Frango", .tempo = 240},
+        {.nome = "Legumes", .tempo = 150},
     };
-    int num_pratos = sizeof(pratos) / sizeof(pratos[0]);
+    int num_pratos=sizeof(pratos)/sizeof(pratos[0]);
     int escolha;
     char entrada[10];
+
     while (1) {
         menu(pratos, num_pratos);
         kprintf("Digite o numero do prato: ");
-        getn(entrada, sizeof(entrada));
-        escolha = atoi(entrada);
+        
+        for (int i = 0; i < sizeof(entrada) - 1; i++) {
+            entrada[i] = getc(CONSOLE);
+            if (entrada[i] == '\n') {
+                entrada[i] = '\0';
+                break;
+            }
+        }
+        escolha=atoi(entrada);
         if (escolha < 1 || escolha > num_pratos) {
             kprintf("Escolha invalida! Tente outro numero.\n");
         } else {
