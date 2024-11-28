505a8b8cd437a3ba519707f1fd7bc324b4c39f71
Félix Netto <112364180+Freuks@users.noreply.github.com>
2024-11-28 15:27:20 -0300

    Update luzinterna.cpp

--- microondas/luzinterna.cpp
+++ microondas/luzinterna.cpp
@@ -1,9 +1,8 @@
 #include <xinu.h>
 
-process main(void) {
-    int buttonState = 0;
-    char inputBuffer[3];
-    bool heating = FALSE;
+shellcmd xsh_luz(int nargs, char *args[]) {
+    char inputBuffer[2];
+    bool isPortOpen = FALSE; 
 
     kprintf("Digite 1 para porta aberta, 0 para porta fechada, e 2 para simular aquecimento:\n");
 
@@ -11,23 +10,27 @@ process main(void) {
         int n = read(CONSOLE, inputBuffer, 1);
         if (n > 0) {
             inputBuffer[n] = '\0';
-            buttonState = inputBuffer[0] - '0';
+            char command = inputBuffer[0];
 
-            if (inputBuffer[0] == '1') {
-                kprintf("Porta aberta.\n");
-                heating = TRUE;
-                kprintf("Luz interna ligada.\n");
-            } else if (inputBuffer[0] == '0') {
-                kprintf("Porta fechada.\n");
-                heating = FALSE;
-                kprintf("Luz interna desligada.\n");
-            } else if (inputBuffer[0] == '2') {
-                kprintf("Aquecendo...\n");
-                heating = FALSE;
-                kprintf("Luz interna ligada.\n");
-            } else {
-                kprintf("VOCE DESTRUIU O MICROONDAS, pressione 1 ou 0.\n");
+            while (inputBuffer[0] != '\n') {
+                read(CONSOLE, inputBuffer, 1);
             }
+            if (command == '1') {
+                kprintf("Porta aberta.\n");
+                isPortOpen = FALSE;
+                kprintf("Luz interna ligada.\n");
+            } else if (command == '0') {
+                kprintf("Porta fechada.\n");
+                isPortOpen = TRUE;
+                kprintf("Luz interna desligada.\n");
+            } else if (command == '2') {
+                if (isPortOpen) {
+                    kprintf("Aquecendo...\n");
+                    kprintf("Luz interna ligada.\n");
+                } else {
+                    kprintf("Aquecimento não pode ocorrer com a porta aberta.\n");
+                }
+            } 
         }
     }
     return OK;
